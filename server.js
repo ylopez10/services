de0f6d3d4a656ac0cabc9295242d884918441633
ylopez <ylopez@novopayment.com>
2023-01-31 12:27:40 -0500

    Ajusta repuestas.

--- server.js
+++ server.js
@@ -9,5 +9,23 @@ server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
+router.render = (req, res) => {
+    let code = res.statusCode
+    switch (code) {
+        case 200:
+            res.jsonp({
+                code: 0,
+                data: res.locals.data
+            })
+            break;
+        default:
+            res.jsonp({
+                code: 1,
+                data: "El dato solicitido no existe"
+            })
+            break;
+    }
+}
+
 // Export the Server API
 module.exports = server
