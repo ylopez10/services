4a0c6bb197031a417d666e6ab44ea507f42dc771
ylopez <ylopez@novopayment.com>
2023-01-31 13:10:19 -0500

    Ajusta manejo de error id duplicado

--- server.js
+++ server.js
@@ -9,10 +9,25 @@ server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
+router.stack = router.stack.filter(item => item.handle.length !== 4)
+
+router.use((err, req, res, next) => {
+  let status = 500;
+  if (err.message === 'Insert failed, duplicate id') {
+    status = 409
+  }
+
+  res.status(status).send({
+    code: 1,
+    data: 'Error en el registro, el id esta duplicado.'
+  })
+})
+
 router.render = (req, res) => {
     let code = res.statusCode
     switch (code) {
         case 200:
+        case 201:
             res.jsonp({
                 code: 0,
                 data: res.locals.data
