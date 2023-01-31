4810f1b08662de4ac82b93ff4f8da62dd0f7cbfb
ylopez <ylopez@novopayment.com>
2023-01-31 11:35:38 -0500

    Ajutsa data

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ const router = jsonServer.router('api.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/cards': '/cards',
-    "/cards/name/:name": "/cards?name_like=:name"
-}))
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
